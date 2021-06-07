3e0f184369d80fddae1c9af48c5b9e696192875c
agarwalshivani860 <63181615+agarwalshivani860@users.noreply.github.com>
2021-06-07 21:17:08 +0530

    Add files via upload

---
+++ Day3/Exercise31/src/com/exceptions/EmployeeMenu.java
@@ -0,0 +1,10 @@
+package com.exceptions;
+
+public class EmployeeMenu {
+ public EmployeeMenu()
+ {
+	 System.out.println("1.Create Employee Record");
+	 System.out.println("2.Display Employee Record");
+	 System.out.println("3.Exit");
+ }
+}
